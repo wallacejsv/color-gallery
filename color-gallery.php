427a8c79e9e4450e52a0c85f22008c94b8ad4b8c
luancuba <luancuba9@gmail.com>
2016-05-12 21:59:49 -0300

    added main file with wp plugin header

---
+++ color-gallery.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Color Gallery
+Plugin URI: https://wordpress.org/plugins/color-gallery/
+Description: Create photo gallery and filter by color
+Version: 1.0
+Author: Luan Cuba
+Author URI: http://luancuba.com.br
+Text Domain: color-gallery
+Domain Path: /languages
+*/
+
